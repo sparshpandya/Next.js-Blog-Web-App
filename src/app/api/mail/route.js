cfdad3527142bc0fb122c86a9996deca3cf05b27
Sparsh Pandya <sparshpandya228@gmail.com>
2024-09-15 01:55:48 -0400

    fixed some errors

--- src/app/api/mail/route.js
+++ src/app/api/mail/route.js
@@ -4,29 +4,31 @@ import NotionMagicLinkEmail from "../../../../emails";
 
 import { Resend } from "resend";
 
-// creating a new resend instance
-const resend = new Resend(process.env.RESEND_API_KEY);
-
 export async function POST(req, res) {
-    try {
-        // getting the user email
-        const { email, token } = await req.json();
-        
-        // sending the email
-      const { data, error } = await resend.emails.send({
-        from: 'Acme <onboarding@resend.dev>',
-        to: [email],
-        subject: 'test email',
-        react: NotionMagicLinkEmail(token),
-      });
-  
-      if (error) {
-        return Response.json({ error }, { status: 500 });
-      }
-      
-    //   returning the success message
-      return Response.json("Verification Email Sent Successfully!!", { status: 201 });
-    } catch (error) {
+  const apiKey = process.env.RESEND_API_KEY;
+  // creating a new resend instance
+  const resend = new Resend(apiKey);
+  try {
+    // getting the user email
+    const { email, token } = await req.json();
+
+    // sending the email
+    const { data, error } = await resend.emails.send({
+      from: "Acme <onboarding@resend.dev>",
+      to: [email],
+      subject: "test email",
+      react: NotionMagicLinkEmail(token),
+    });
+
+    if (error) {
       return Response.json({ error }, { status: 500 });
     }
+
+    //   returning the success message
+    return Response.json("Verification Email Sent Successfully!!", {
+      status: 201,
+    });
+  } catch (error) {
+    return Response.json({ error }, { status: 500 });
   }
+}
